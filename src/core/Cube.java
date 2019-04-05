1bc12c6283172dce61e601b24876ec46ee67f6b7
Bruno Wandrey <mababa3003@gmail.com>
2019-04-05 17:29:39 -0300

    First commit

---
+++ src/core/Cube.java
@@ -0,0 +1,18 @@
+package core;
+
+public class Cube {
+
+
+
+    public void rotateFrontFaceClockwise(){
+
+    }
+    public void rotateFrontFaceCounterClockwise(){
+
+    }
+
+    public void rotateCube(Char axis, integer direction){
+
+
+    }
+}
